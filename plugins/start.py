0633d119b8a58f114447cf635142b97fbb1e9ef0
SageBots <112871122+SageBots@users.noreply.github.com>
2022-09-06 20:30:12 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -19,12 +19,7 @@ async def start(client,message):
 
 ☞ Then Wait Till The Process Get Completed!
 
-☞ Maintained By : @ChVivekTomar
-
-**☞ Want To Buy Owr Premium Rename Bot With Custom Caption Support Contact Me On @ChVivekTomar**
-	""",reply_to_message_id = message.message_id ,  
-	reply_markup=InlineKeyboardMarkup(
-	 [[ InlineKeyboardButton("Vivek Projects" ,url="https://t.me/OfficialVivekProject") ]  ]))
+☞ Maintained By : @SageBots
 
 
 @Client.on_message(filters.private &( filters.document | filters.audio | filters.video ))
